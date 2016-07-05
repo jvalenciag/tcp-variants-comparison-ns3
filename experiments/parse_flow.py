7cbf25771e6d5810f291026ca6a0c45fd890ab01
jvalenciag <taurus.936@gmail.com>
2016-07-05 11:27:32 -0300

    add defult simulation data

--- experiments/parse_flow.py
+++ experiments/parse_flow.py
@@ -3,6 +3,12 @@ import xml.etree.ElementTree as ET
 import shlex, subprocess
 import numpy as np
 
+results_prefix = "results/"
+data_prefix = "data/"
+per_file_name = "per_comp.data"
+delay_file_name = "delay_comp.data"
+file_name = "default_comp.data"
+
 protocols=["TcpTahoe","TcpNewReno", "TcpCubic", "TcpWestwoodPlus"]
 pers = [1e-4,1e-3,1e-2,0.05,0.07]
 delays = [0.01,50.0,100.0,150.0,200.0,250.0]
@@ -14,9 +20,9 @@ default_duration = 120
 def sizeof_fmt(num, suffix='bps'):
     for unit in ['','K','M','G','T','P','E','Z']:
         if abs(num) < 1000.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
+            return '{:3.1f}{}{}'.format(num, unit, suffix)
         num /= 1000.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
+    return '{:.1f}{}{}'.format(num, 'Yi', suffix)
 
 def read_ns_to_sec(num_str):
 	return float(num_str[:-2])/1e+9
@@ -26,11 +32,9 @@ def parse_flowmonitor(filename):
 	root = tree.getroot()
 	flowstat = root[0]
 	flow = flowstat[0]
-	#for flow in flowstat:
-	#print( "=========================================")
-	#print( "Flow Nr "+str(flow.attrib["flowId"]))
-	#print( "=========================================")
+	
 	ret = {}
+	ret["flow_id"] = int(flow.attrib["flowId"])
 
 	delay_sum = read_ns_to_sec(flow.attrib["delaySum"])
 	rx_packets = float(flow.attrib["rxPackets"])
@@ -62,11 +66,6 @@ def parse_flowmonitor(filename):
 	ret["pckt_lost_ratio"]= lost_pckts/(rx_packets+lost_pckts)
 	return ret
 
-results_prefix = "results/"
-data_prefix = "data/"
-per_file_name = "per_comp.data"
-delay_file_name = "delay_comp.data"
-
 def gen_stats(file_prefix):
 	ret = []
 	for prot in protocols:
@@ -75,32 +74,42 @@ def gen_stats(file_prefix):
 		#print(stats)
 	return ret;
 
-def write_stats_to_file(file, first, data=[]):
-	file.write("%s " % first)
-	for item in data:
-		file.write("%s " % item)
-	file.write("\n")
+def write_stats_to_file(file, data=[]):
+	tmp = [str(x) for x in data]
+	data_str = " ".join(tmp)+"\n"
+	file.write(data_str)
 
 print("Runing cwnd tests")
+
+
 #proc = subprocess.run(["./run_tests.sh", str(default_duration), str(default_per), str(default_delay)+"ms",data_prefix ], stdout=subprocess.PIPE)
+data = gen_stats(data_prefix)
+file = open(results_prefix+file_name,'w')
+
+write_stats_to_file(file, data)
+
+tmp = [sizeof_fmt(x) for x in data]
+data_str = " ".join(tmp) + "\n"	
+file.write(data_str)
+
+file.close()
 
 print("Runing per tests")
 per_file = open(results_prefix+per_file_name, 'w')
 for error_p in pers:
 	file_prefix = data_prefix+"per_"+'{:.0e}'.format(error_p)+"-"
 	#proc = subprocess.run(["./run_tests.sh", str(default_duration), str(error_p), str(default_delay)+"ms",file_prefix ], stdout=subprocess.PIPE)
-	#print(proc.stdout)
 	data = gen_stats(file_prefix)
-	write_stats_to_file(per_file, error_p,data)
-
+	data.insert(0, error_p)
+	write_stats_to_file(per_file, data)
 per_file.close()
 
 print("Runing delay tests")
 delay_file = open(results_prefix+delay_file_name, 'w')
 for d in delays:
 	file_prefix = data_prefix+"delay_"+str(d)+"-"
-	proc = subprocess.run(["./run_tests.sh", str(default_duration), str(default_per), str(d)+"ms", file_prefix ], stdout=subprocess.PIPE)
-	#print(proc.stdout)
+	#proc = subprocess.run(["./run_tests.sh", str(default_duration), str(default_per), str(d)+"ms", file_prefix ], stdout=subprocess.PIPE)
 	data = gen_stats(file_prefix)
-	write_stats_to_file(delay_file, d, data)
+	data.insert(0, d)
+	write_stats_to_file(delay_file, data)
 delay_file.close()
